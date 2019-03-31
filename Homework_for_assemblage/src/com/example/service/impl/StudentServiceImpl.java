a9f1d180aa1cd53519eb25babf0554a3345f6c09
Rice <ricemarch@foxmai.com>
2019-03-31 20:18:37 +0800

    集合、单例对象

---
+++ Homework_for_assemblage/src/com/example/service/impl/StudentServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.service.impl;
+
+import com.example.entity.Student;
+import com.example.resource.Database;
+import com.example.service.StudentService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public  class StudentServiceImpl implements StudentService {
+
+    @Override
+    public List<Student> listStudents(){//记得要写成public类型
+        Database stus =  new Database();
+        return stus.getTpstus();//返回临时数据库中的全部学生
+    }
+    @Override
+    public void addStudent(Student stu){
+       Database.getTpstus().add(stu);
+    }
+    @Override
+    public Student getStudent(int sid){
+        Database stus =  new Database();
+        stus.getTpstus();
+        Student temp = new Student(0, "NULL");
+        for (Student tp:stus.getTpstus()) {
+            if (tp.getId()== sid) {
+                System.out.println("找到了该学生：" );
+                temp = tp;
+                break;
+            }
+        }
+        return temp;//返回查找到的学生
+    }
+    @Override
+    public List<Student> listStudentsByYear(int year){
+        List<Student> gardestus = new ArrayList<Student>();
+        Database stus = new Database();//读取基本临时数据库
+        String year_ = String.valueOf(year);
+        for (Student tp:stus.getTpstus()) {
+           String tempid = String.valueOf(tp.getId());
+            //       System.out.println(tempid.substring(0,4)+" "+year_);
+            if (tempid.substring(0,4).equals(year_)) {//记得使用equal方法！！ 不能用“==”直接比较！
+            gardestus.add(tp);
+            }
+        }
+        return gardestus;
+    }
+}
