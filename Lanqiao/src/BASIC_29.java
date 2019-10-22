065f54440ec9651739949608c8fbf0a2c16f42d2
Rice <ricemarch@foxmail.com>
2019-10-22 22:43:39 +0800

    大数相加

---
+++ Lanqiao/src/BASIC_29.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class BASIC_29 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int[] a = new int[101];
+        int[] b = new int[101];
+        int[] result = new int[101];
+        for (int i = 0; i < 101; i++) {
+            a[i] = b[i] = result[i] = 0;
+        }
+        String line1 = cin.nextLine();
+        String line2 = cin.nextLine();
+
+        for (int i = 0; i < line1.length(); i++)
+            a[i] = (int) line1.charAt(line1.length() - 1 - i) - 48;
+        for (int i = 0; i < line2.length(); i++)
+            b[i] = (int) line2.charAt(line2.length() - 1 - i) - 48;
+
+        int max=line1.length()>line2.length()?line1.length():line2.length();
+
+        int jinwei=0;
+        int r=0;
+        for (int i = 0; i < 101; i++)
+        {
+            result[i]=a[i]+b[i]+r;
+            r=result[i]/10;
+            result[i]%=10;
+        }
+        int flag=100;
+        for (int i = 100; i>-1; i--) {
+            if (result[i]!=0) {
+                flag=i;
+                break;
+            }
+        }
+        for (int i = flag; i >-1; i--) {
+            System.out.print(result[i]);
+        }System.out.println();
+    }
+}
