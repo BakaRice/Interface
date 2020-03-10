7a0f7d873fb89703828ee8ff12d6be9619757a56
Rice <ricemarch@foxmail.com>
2020-03-10 16:48:00 +0800

    项目结构重构2 删除冗余

--- algorithm/Newcoder3/src/test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        int i[][]={{1,2,3},{2,3},{1},{2,3}};
-        System.out.println(i[0].length);
-        System.out.println(i.length);
-    }
-}
